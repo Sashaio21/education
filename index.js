1502539d826346d2c49e01db4a29f28610790599
Sascha <your.email@example.com>
2024-09-20 01:57:03 +0300

    first commit

--- index.js
+++ index.js
@@ -1,7 +1,8 @@
-let number = Math.floor(Math.random() * 10 - 5)
+let one_word = "Первое слово"
+let two_word = "Второе слово"
 
-console.log(number)
-
-if (number < 0) {
-    console.log("Число отрицательное")
+if (one_word[0] == two_word[0]) {
+    console.log("Первые буквы совпадают")
+} else {
+    console.log("Первые буквы не совпадают")
 }
